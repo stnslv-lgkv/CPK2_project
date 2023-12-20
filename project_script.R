e9b68e9b907486980c41bbc7f9518bf34102da95
Stanislav Legkovoy <stanislavlegkovoy@yahoo.com>
2023-12-20 18:58:14 +0300

    fixed merge

--- project_script.R
+++ project_script.R
@@ -14,13 +14,11 @@ fb140_stats <- openxlsx::read.xlsx("./data/final_ibs_140_statistic.xlsx")       
 ## processing 'bac_gr_funcs' dt
 
 bac_gr_funcs <- bac_gr_funcs %>% 
-  mutate(Bacteria_tax = paste(TaxonName, Rank, sep = "_"),
-         Bacteria_category_eng = case_when(
-           Bacteria_category == "Условно-нормальный" ~ "Non-beneficial", 
-           Bacteria_category == "Патоген" ~ "Pathogenic",
-           .default = NA_character_)
-  )
-
+  mutate(Inflammatory = coalesce(Inflammatory, 0),
+         Oral = coalesce(Oral, 0),
+         Gases = coalesce(Gases, 0),
+         TaxonName = gsub(" ", "_", TaxonName),
+         Bacteria_tax = paste(TaxonName, Rank, sep = "_"))
 
 ## make dts long
 
@@ -39,11 +37,11 @@ fbh_long <- pivot_long_foo(fbh)
 ## merge dts(fb40_long/fbh_long with corresponding stats dts)
 
 fb140_mrg <- fb140_long %>% 
-  full_join(fb140_stats, by="patient_ID") %>% 
+  inner_join(fb140_stats, by="patient_ID") %>% 
   mutate(source = "IBS")
 
 fbh_mrg <- fbh_long %>% 
-  full_join(fbh_stats, by="patient_ID") %>% 
+  inner_join(fbh_stats, by="patient_ID") %>% 
   mutate(source = "Healthy")
 
 ## set fb40/fbh dts together 
@@ -53,7 +51,7 @@ fb_long <- bind_rows(fbh_mrg, fb140_mrg)
 ## left join fb_long with bac_gr_funcs
 
 fb_all <- fb_long %>% 
-  left_join(bac_gr_funcs, by="Bacteria_tax")
+  inner_join(bac_gr_funcs, by="Bacteria_tax")
 
 # NA_count_foo <- function(dt) {
 #   dt %>% 
@@ -69,25 +67,30 @@ NA_count_foo <- function(dt) {
     summarise(across(everything(), ~ sum(is.na(.x))),
               n = n()) %>% 
     select(n, where(function(x) x != 0)) %>% 
-    mutate(across(everything()), round(. / n, 2)) %>% 
-    select(-n)
+    mutate(across(everything()), round(. / n, 4)) %>% 
+    select(-n) %>% 
+    pivot_longer(
+      cols = everything(),
+      names_to = "Feature",
+      values_to = "Value"
+    ) %>% 
+    arrange(desc(Value))
 }
 
-NA_cols_foo <- function(dt) {
-  dt %>% 
-    select(patient_ID, where(~sum(is.na(.x)) > 0))
-}
+# NA_cols_foo <- function(dt) {
+#   dt %>% 
+#     select(patient_ID, where(~sum(is.na(.x)) > 0))
+# }
 
 ## save temporary dataset
-
-data.table::fwrite(fb_all, "./output/all_long.csv")
+# data.table::fwrite(fb_all, "./output/all_long.csv")
 
 fb_all_NA_cnt <- NA_count_foo(fb_all)
-fb_all_NA_dt <- NA_cols_foo(fb_all)
 
+# fb_all_NA_dt <- NA_cols_foo(fb_all)
 
 ## dt with non-empty Bacteria_category column 
-
-tmp <- fb_all %>% 
-  filter(!is.na(Bacteria_category_eng))
+# 
+# tmp <- fb_all %>% 
+#   filter(!is.na(Bacteria_category_eng))
 
